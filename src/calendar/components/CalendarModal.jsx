6ffa70bc38792487261ca1dfd58e65c6a1b8323b
Lilopadilla22 <lilopadilla22@gmail.com>
2023-05-27 17:51:49 -0500

    add content-modal

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -1,3 +1,4 @@
+import { addHours } from 'date-fns';
 import React, { useState } from 'react'
 import Modal from 'react-modal'
 
@@ -15,6 +16,13 @@ const customStyles = {
 Modal.setAppElement('#root');
 
 export const CalendarModal = () => {
+
+    const [formValues, setFormValues] = useState({
+        title: 'Lilia',
+        notes: 'Padilla',
+        start: new Date(),
+        end: addHours(new Date(), 2)
+    })
     
     const [isOpen, setIsOpen] = useState(true)
 
@@ -32,10 +40,53 @@ export const CalendarModal = () => {
         overlayClassName='modal-fondo'
         closeTimeoutMS={200}
     >
-        <h1>Hola Mundo</h1>
-        <hr/>
-        <p>Lorem, ipsum dolor sit.</p>
+        <h1> Nuevo evento </h1>
+        <hr />
+        <form className="container">
 
+            <div className="form-group mb-2">
+                <label>Fecha y hora inicio</label>
+                <input className="form-control" placeholder="Fecha inicio" />
+            </div>
+
+            <div className="form-group mb-2">
+                <label>Fecha y hora fin</label>
+                <input className="form-control" placeholder="Fecha inicio" />
+            </div>
+
+            <hr />
+            <div className="form-group mb-2">
+                <label>Titulo y notas</label>
+                <input 
+                    type="text" 
+                    className="form-control"
+                    placeholder="Título del evento"
+                    name="title"
+                    autoComplete="off"
+                />
+                <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
+            </div>
+
+            <div className="form-group mb-2">
+                <textarea 
+                    type="text" 
+                    className="form-control"
+                    placeholder="Notas"
+                    rows="5"
+                    name="notes"
+                ></textarea>
+                <small id="emailHelp" className="form-text text-muted">Información adicional</small>
+            </div>
+
+            <button
+                type="submit"
+                className="btn btn-outline-primary btn-block"
+            >
+                <i className="far fa-save"></i>
+                <span> Guardar</span>
+            </button>
+
+        </form>
     </Modal>
   )
 }
