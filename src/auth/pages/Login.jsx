6168e9b3d79f8403192b1dfc9cecc5f7819cc96c
Lilia Padilla <lilopadilla22@gmail.com>
2023-06-03 20:02:32 -0500

    add useform, axios and variables

--- src/auth/pages/Login.jsx
+++ src/auth/pages/Login.jsx
@@ -1,18 +1,49 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
 import './login.css';
+import { useForm } from '../../hooks/useForm';
+
+const loginFormFields = {
+    loginEmail: "",
+    loginPassword: ""
+}
+
+const registerFormFields = {
+    registerName: "",
+    registerEmail: "",
+    registerPassword: "",
+    registerPassword2: ""
+}
 
 export const Login = () => {
+
+    const { loginEmail, loginPassword, onInputChange: onLoginInputchange } = useForm(loginFormFields)
+    const { registerName, registerEmail, registerPassword, registerPassword2, onInputChange: onRegisterInputChange } = useForm(registerFormFields)
+
+    const loginSubmit = (event) => {
+        event.preventDefault()
+        console.log({ loginEmail, loginPassword })
+    }
+
+    const registerSubmit = (event) => {
+        event.preventDefault()
+        console.log({ registerName, registerEmail, registerPassword, registerPassword2 })
+    }
+
     return (
         <div className="container login-container">
             <div className="row">
                 <div className="col-md-6 login-form-1">
                     <h3>Ingreso</h3>
-                    <form>
+                    <form onSubmit={loginSubmit}>
                         <div className="form-group mb-2">
-                            <input 
+                            <input
                                 type="text"
                                 className="form-control"
                                 placeholder="Correo"
+                                name="loginEmail"
+                                value={loginEmail}
+                                onChange={onLoginInputchange}
                             />
                         </div>
                         <div className="form-group mb-2">
@@ -20,26 +51,31 @@ export const Login = () => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Contraseña"
+                                name="loginPassword"
+                                value={loginPassword}
+                                onChange={onLoginInputchange}
                             />
                         </div>
                         <div className="form-group mb-2">
-                            <input 
+                            <input
                                 type="submit"
                                 className="btnSubmit"
-                                value="Login" 
+                                value="Login"
                             />
                         </div>
                     </form>
                 </div>
-
                 <div className="col-md-6 login-form-2">
                     <h3>Registro</h3>
-                    <form>
+                    <form onSubmit={registerSubmit}>
                         <div className="form-group mb-2">
                             <input
                                 type="text"
                                 className="form-control"
                                 placeholder="Nombre"
+                                name="registerName"
+                                value={registerName}
+                                onChange={onRegisterInputChange}
                             />
                         </div>
                         <div className="form-group mb-2">
@@ -47,28 +83,35 @@ export const Login = () => {
                                 type="email"
                                 className="form-control"
                                 placeholder="Correo"
+                                name="registerEmail"
+                                value={registerEmail}
+                                onChange={onRegisterInputChange}
                             />
                         </div>
                         <div className="form-group mb-2">
                             <input
                                 type="password"
                                 className="form-control"
-                                placeholder="Contraseña" 
+                                placeholder="Contraseña"
+                                name="registerPassword"
+                                value={registerPassword}
+                                onChange={onRegisterInputChange}
                             />
                         </div>
-
                         <div className="form-group mb-2">
                             <input
                                 type="password"
                                 className="form-control"
-                                placeholder="Repita la contraseña" 
+                                placeholder="Repita la contraseña"
+                                name="registerPassword2"
+                                value={registerPassword2}
+                                onChange={onRegisterInputChange}
                             />
                         </div>
-
                         <div className="form-group mb-2">
-                            <input 
-                                type="submit" 
-                                className="btnSubmit" 
+                            <input
+                                type="submit"
+                                className="btnSubmit"
                                 value="Crear cuenta" />
                         </div>
                     </form>
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { Login } from '../auth/pages/Login'
 import { CalendarPage } from '../calendar/pages/Calendar'
 
 export const AppRouter = () => {
 
-    const authState= 'authenticated'
+    const authState= 'not-authenticated'
 
     return (
         <Routes>
